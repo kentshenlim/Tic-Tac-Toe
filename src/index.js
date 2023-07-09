998b8a36229c84db66f2c076ed8ee91acf7ca802
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-09 21:11:31 +0100

    Tidy up index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import gameBoard from './gameBoard';
-import logic from './logic';
+import './gameBoard';
+import './logic';
+import './ui';
 import './style.css';
-import ui from './ui';
-
-gameBoard.exposeGrid();
-
-// Expose interface functions
-window.gameBoard = { pickGrid: gameBoard.pickGrid, resetGrid: gameBoard.resetGrid };
