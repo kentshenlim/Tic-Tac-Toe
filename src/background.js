3ce9cda4069a68ce446a9da2b0529342c4696298
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-11 21:18:04 +0100

    Add motif functions

--- src/background.js
+++ src/background.js
@@ -3,6 +3,12 @@ import xImg from './img/x.png';
 import oImg from './img/o.png';
 
 (() => {
+  // Cache DOM
+  const body = document.querySelector('body');
+
+  const background = document.createElement('div');
+  background.classList.add('background-wrapper');
+
   // Method declaration
   function createImgNode(imgSrc, classNameList) {
     const imgNode = document.createElement('img');
@@ -13,15 +19,27 @@ import oImg from './img/o.png';
     return imgNode;
   }
 
-  // Cache DOM
-  const body = document.querySelector('body');
+  function insertMotif(imgSrc, classNameList, r, c) {
+    // Insert r by c
+    const verticalGap = 100 / r;
+    const horizontalGap = 100 / c;
+    for (let i = 0; i < r; i += 1) {
+      for (let j = 0; j < c; j += 1) {
+        const node = createImgNode(imgSrc, classNameList);
+        node.style.width = '10%';
+        node.style.position = 'absolute';
+        node.style.top = `${verticalGap * i}%`;
+        node.style.left = `${horizontalGap * j}%`;
+        background.appendChild(node);
+      }
+    }
+  }
 
-  const background = document.createElement('div');
-  background.classList.add('background-wrapper');
+  insertMotif(oImg, [], 3, 5);
 
-  const imgTest = document.createElement('img');
-  imgTest.src = oImg;
-  background.appendChild(createImgNode(oImg, []));
+  //   const imgTest = document.createElement('img');
+  //   imgTest.src = oImg;
+  //   background.appendChild(createImgNode(oImg, []));
 
   body.appendChild(background);
 })();
