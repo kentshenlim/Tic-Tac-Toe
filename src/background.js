8207ab01b4281e92c4807c998bca8cf0e8e900d6
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-11 20:47:15 +0100

    Cute animation

--- src/background.js
+++ src/background.js
@@ -1,1 +1,16 @@
 import './background.css';
+import xImg from './img/x.png';
+import oImg from './img/o.png';
+
+(() => {
+  const body = document.querySelector('body');
+
+  const background = document.createElement('div');
+  background.classList.add('background-wrapper');
+
+  const imgTest = document.createElement('img');
+  imgTest.src = oImg;
+  background.appendChild(imgTest);
+
+  body.appendChild(background);
+})();
