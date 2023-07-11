0f4250effd3bbc27c3d156bdeb1904f81e9961b8
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-11 21:08:23 +0100

    Finalize for oAnimation

--- src/background.js
+++ src/background.js
@@ -3,6 +3,17 @@ import xImg from './img/x.png';
 import oImg from './img/o.png';
 
 (() => {
+  // Method declaration
+  function createImgNode(imgSrc, classNameList) {
+    const imgNode = document.createElement('img');
+    imgNode.src = imgSrc;
+    classNameList.forEach((className) => {
+      imgNode.classList.add(className);
+    });
+    return imgNode;
+  }
+
+  // Cache DOM
   const body = document.querySelector('body');
 
   const background = document.createElement('div');
@@ -10,7 +21,7 @@ import oImg from './img/o.png';
 
   const imgTest = document.createElement('img');
   imgTest.src = oImg;
-  background.appendChild(imgTest);
+  background.appendChild(createImgNode(oImg, []));
 
   body.appendChild(background);
 })();
